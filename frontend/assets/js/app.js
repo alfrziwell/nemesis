cf0095d7bdc6aee4da56bace6548713ca5afdf28
Ghaida zalfa <ghaidagabang17@gmail.com>
2026-05-02 15:00:41 +0700

    asdas

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -1,5 +1,5 @@
 (() => {
-  const API_BASE_URL = (window.DASHBOARD_API_BASE_URL || "http://127.0.0.1:3000/api").replace(/\/$/, "");
+  const API_BASE_URL = ("https://backend-kelompok-4.vercel.app/api" || window.DASHBOARD_API_BASE_URL || "http://127.0.0.1:3000/api").replace(/\/$/, "");
 
   if (!window.maplibregl || !window.AuditMap) {
     console.error("MapLibre GL or AuditMap failed to load.");
